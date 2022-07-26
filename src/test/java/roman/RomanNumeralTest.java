4ac7133ce64b9a4e9ce5367a2ad86b42fae54dcf
Virjanand Panday <virjanand@virjanand.com>
2022-07-26 18:58:37 +0200

    Refactor to parameterized test to make adding new tests easier

--- src/test/java/roman/RomanNumeralTest.java
+++ src/test/java/roman/RomanNumeralTest.java
@@ -1,20 +1,19 @@
 package roman;
 
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class RomanNumeralTest {
 
-    @Test
-    void convert1ToI() {
-        RomanNumber romanNumber = new RomanNumber(1);
-        assertThat(romanNumber.toRoman()).isEqualTo("I");
-    }
-
-    @Test
-    void convert2ToII() {
-        RomanNumber romanNumber = new RomanNumber(2);
-        assertThat(romanNumber.toRoman()).isEqualTo("II");
+    @ParameterizedTest(name = "{0} -> {1}")
+    @CsvSource({
+            "1, I",
+            "2, II",
+    })
+    void convertArabicToRomanNumber(int arabicNumber, String romanNumberString) {
+        RomanNumber romanNumber = new RomanNumber(arabicNumber);
+        assertThat(romanNumber.toRoman()).isEqualTo(romanNumberString);
     }
 }
