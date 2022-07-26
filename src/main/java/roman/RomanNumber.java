a17277d130358d700148955a2d5da4cf83edefe5
Virjanand Panday <virjanand@virjanand.com>
2022-07-26 19:11:10 +0200

    Refactor to potential general solution

--- src/main/java/roman/RomanNumber.java
+++ src/main/java/roman/RomanNumber.java
@@ -9,12 +9,17 @@ public class RomanNumber {
 
     public String toRoman() {
         String result = "";
-        if (arabicNumber == 4) {
-            result += "IV";
-            arabicNumber -= 4;
-        }
-        if (arabicNumber >= 1) {
-            result += "I".repeat(arabicNumber);
+        result += extractRomanFromArabicNumber(4, "IV");
+        result += extractRomanFromArabicNumber(1, "I");
+        return result;
+    }
+
+    private String extractRomanFromArabicNumber(int arabicNumber, String romanNumberString) {
+        int timesNumberFitsInArabicNumber = this.arabicNumber / arabicNumber;
+        String result = "";
+        if (this.arabicNumber >= 1) {
+            result += romanNumberString.repeat(timesNumberFitsInArabicNumber);
+            this.arabicNumber -= this.arabicNumber * timesNumberFitsInArabicNumber;
         }
         return result;
     }
