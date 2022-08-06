db75821de8bd595c2adbee2bc04b06bb03a79e76
Virjanand Panday <virjanand@virjanand.com>
2022-08-06 12:32:11 +0200

    Extract method

--- src/test/java/roman/RomanToArabicNumberConverter.java
+++ src/test/java/roman/RomanToArabicNumberConverter.java
@@ -9,10 +9,15 @@ public class RomanToArabicNumberConverter {
 
     public int toArabic() {
         int result = 0;
-        result += 4 * (romanNumberString.split("IV", -1).length - 1);
-        romanNumberString = romanNumberString.replaceAll("IV", "");
-        result += 5 * (romanNumberString.split("V", -1).length - 1);
-        result += (romanNumberString.split("I", -1).length - 1);
+        result += countAmountFrom("IV", 4);
+        result += countAmountFrom("V", 5);
+        result += countAmountFrom("I", 1);
+        return result;
+    }
+
+    private int countAmountFrom(String romanNumberPart, int arabicValue) {
+        int result = arabicValue * (romanNumberString.split(romanNumberPart, -1).length - 1);
+        romanNumberString = romanNumberString.replaceAll(romanNumberPart, "");
         return result;
     }
 }
